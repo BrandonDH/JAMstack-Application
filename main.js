014e42835b520060780fdb3e403a02441b8326cd
Brandon Herford <brandonherford@Brandons-Air.fios-router.home>
2020-03-16 19:47:25 -0400

    Add fix to main.js

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const listRepos = async username => {
   const repos = await fetch(
-    'https://api.github.com/users/${username}/repos?owner&sort=updated'
+    `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
   )
     .then(res => res.json())
     .catch(error => console.log(error))
 
     const markup = repos
       .map(
-        repo =>`
+        repo => `
         <li>
         <a href="${repo.html_url}">${repo.name}</a>
         (🥶 ${repo.stargazers_count})
         </li>
         `
       )
-      .join('')
+      .join('');
 
 const content = document.getElementById('content');
 
-content.innerHTML = `<ul>${markup}</li>`;
-};
+content.innerHTML = `<ul>${markup}</ul>`;
+}
 
-listRepos('brandondh');
+listRepos('BrandonDH');
